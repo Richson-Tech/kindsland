30436aed015f8ce2b2f62278bfc5e7508a5981a5
Richson-Tech <folorunshoahmedolajide@gmail.com>
2023-11-08 22:24:02 +0100

    image resize

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div>
-      <Image src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXN0YXRlfGVufDB8fDB8fHww"
-      height={600}
-      width={600}
-      />
-    </div>
-  )
-}
+    <main>
+      <div className="h-max max-w-full">
+        <Image
+          src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?auto=format&fit=crop&q=60&w=800&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZXN0YXRlfGVufDB8fDB8fHww"
+          height={200}
+          width={1800}
+        />
+      </div>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
